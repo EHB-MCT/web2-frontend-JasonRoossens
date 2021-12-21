aab132002ac829d51185ab26ffda532afec9873c
JasonRoossens <jason.roossens@student.ehb.be>
2021-12-21 16:17:17 +0100

    show data from database + add data to database

---
+++ JAVASCRIPT/index.js
@@ -0,0 +1,96 @@
+let challengesList;
+let challengesHTML = "";
+
+window.onload = function () {
+
+    const insertForm = document.getElementById("uploadForm")
+
+    async function createChallenge(event) {
+
+        //Get the data from the form fields
+        let challengeName = document.getElementById('name').value;
+        let challengePts = document.getElementById('points').value;
+        let challengeCourse = document.getElementById('course').value;
+        let challengeSession = document.getElementById('session').value;
+
+        fetch(`https://web2-courseproject-jason.herokuapp.com/dogs`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                name: challengeName,
+                points: challengePts,
+                course: challengeCourse,
+                session: challengeSession
+            })
+
+        }).then(response => {
+            return response.json()
+        }).then(async data => {
+            console.log('Success:', data);
+            //Add succes message
+
+            //Update list
+            await renderChallenges();
+        })
+    }
+
+    // When clicking submit on the form
+    insertForm.addEventListener('submit', event => {
+        event.preventDefault();
+
+        createChallenge(event);
+
+    });
+
+    //Add listener to challenge list
+    document.getElementById('challengeList').addEventListener('click', (event) => {
+        //Keep searching for the parent node to register the correct click
+        const challengeId = event.target.closest('.challenge').id;
+        console.log(event.target)
+
+        if (challengeId) {
+            if (event.target.className.indexOf('edit') !== -1) {
+                console.log('edit')
+
+            }
+
+            if (event.target.className.indexOf('trash') !== -1) {
+                console.log('trash')
+            }
+
+        }
+
+    })
+
+    async function renderChallenges() {
+        // Clean previous list
+        challengesHTML = '';
+
+        let resp = await fetch('https://web2-courseproject-jason.herokuapp.com/dogs');
+        let data = await resp.json();
+
+        data.forEach(challenge => {
+            challengesHTML += `<div class="row challenge" 
+            id="${challenge._id}">   
+            <div class="col-10">
+                <p>${challenge.name} - ${challenge.generation} - ${challenge.breed}</p>
+            </div>
+            <div class="col-1 edit">
+                <i class="fas fa-edit"></i> 
+            </div>                         
+            <div class="col-1 trash">
+                <i class="fas fa-trash"></i>
+            </div>
+        </div>`
+        })
+
+        document.getElementById("challengeList").innerHTML = challengesHTML;
+
+    }
+
+    renderChallenges();
+
+
+}
---
+++ JAVASCRIPT/main.js
@@ -0,0 +1,35 @@
+(() => {
+    let e = "";
+    window.onload = function () {
+        async function n() {
+            e = "";
+            let n = await fetch("https://web2-courseproject-jason.herokuapp.com/dogs");
+            (await n.json()).forEach((n => {
+                e += `<div class="column show id="${n._id} ">   \n   <img src="./IMAGES/max.png" alt="Mountains" style="width:100%">\n         <h1>${n.name}</h1> <h2>${n.breed} - ${n.generation}</h2>\n     <button>wijzig</button>    <button>verwijder</button   </div>\n                          \n                 </div>`, `<div id="${n._id}">   \n   <img src="./IMAGES/max.png" alt="Mountains" style="width:100%">\n         <h1>${n.name}</h1> <h2>${n.breed} -${n.generation}</h2>\n            </div>\n           </div>`
+            })), document.getElementById("challengeList").innerHTML = e
+        }
+        document.getElementById("uploadForm").addEventListener("submit", (e => {
+            e.preventDefault(), async function (e) {
+                let t = document.getElementById("name").value,
+                    a = document.getElementById("generation").value,
+                    s = document.getElementById("breed").value
+                fetch("https://web2-courseproject-jason.herokuapp.com/dogs", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({
+                        name: t,
+                        generation: a,
+                        breed: s,
+                    })
+                }).then((e => e.json())).then((async e => {
+                    console.log("Success:", e), await n()
+                }))
+            }()
+        })), document.getElementById("challengeList").addEventListener("click", (e => {
+            const n = e.target.closest(".challenge").id;
+            console.log(e.target), n && (-1 !== e.target.className.indexOf("edit") && console.log("edit"), -1 !== e.target.className.indexOf("trash") && console.log("trash"))
+        })), n()
+    }
+})();
