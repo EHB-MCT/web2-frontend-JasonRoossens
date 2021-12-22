45da6aeb5d13dec2e757cacb8506dd3e9aec391a
JasonRoossens <jason.roossens@student.ehb.be>
2021-12-22 16:18:07 +0100

    Added dog specific page

--- docs/JAVASCRIPT/main.js
+++ docs/JAVASCRIPT/main.js
@@ -16,7 +16,7 @@ function fetchDogs() {
             e = "";
             let n = await fetch("https://web2-courseproject-jason.herokuapp.com/dogs");
             (await n.json()).forEach((n => {
-                e += `<div class="column show id="${n._id} ">   \n   <img src="${"./IMAGES/max.png"}" alt="Mountains" style="width:100%">\n         <h1>${n.name}</h1> <h2>${n.breed} - ${n.generation}</h2>\n     <button class="edit"><i class="fas fa-edit"></i></button>    <button class="trash"> <i class="fas fa-trash"></i></button   </div>\n                          \n                 </div>`, `<div id="${n._id}">   \n   <img src="./IMAGES/max.png" alt="Mountains" style="width:100%">\n         <h1>${n.name}</h1> <h2>${n.breed} -${n.generation}</h2>\n            </div>\n           </div>`
+                e += `<div class="column show id="${n._id} ">   \n <a href="./PAGES/dog.html">   <img src="${"./IMAGES/max.png"}" alt="Mountains" style="width:100%">\n         <h1>${n.name}</h1> <h2>${n.breed} - ${n.generation}</h2>\n </a>    <button class="edit"><i class="fas fa-edit"></i></button>    <button class="trash"> <i class="fas fa-trash"></i></button   </div>\n                          \n                 </div>`, `<div id="${n._id}">   \n   <img src="./IMAGES/max.png" alt="Mountains" style="width:100%">\n         <h1>${n.name}</h1> <h2>${n.breed} -${n.generation}</h2>\n            </div>\n           </div>`
             })), document.getElementById("challengeList").innerHTML = e
         }
         document.getElementById("uploadForm").addEventListener("submit", (e => {
--- docs/JAVASCRIPT/script.js
+++ docs/JAVASCRIPT/script.js
@@ -53,3 +53,20 @@ for (var i = 0; i < btns.length; i++) {
         this.className += " active";
     });
 } */
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
