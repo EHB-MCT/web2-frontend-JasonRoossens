7b974eeea7cb2cba842248b42abda68117e4ed7b
JasonRoossens <jason.roossens@student.ehb.be>
2021-12-23 18:49:37 +0100

    Fetching data from dog api

--- docs/JAVASCRIPT/script.js
+++ docs/JAVASCRIPT/script.js
@@ -58,3 +58,32 @@ for (i = 0; i < acc.length; i++) {
         }
     });
 }
+
+function fetchDogs() {
+
+    fetch('https://api.thedogapi.com/v1/breeds')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            this.data = data;
+
+        });
+}
+
+function showDogNames() {
+    var d1 = document.getElementById('example');
+    d1.insertAdjacentHTML('afterend',
+        `<div class="name">${this.data[5].name}</div>
+        <div class="weight">Gewicht: ${this.data[5].weight.metric} kg</div>
+        <div class="weight">Grootte: ${this.data[5].height.metric} cm</div>
+    <img class="picture" src="${this.data[5].image.url}" alt="picture of a dog in object 5 of array">
+    
+    `);
+}
+
+
+
+window.onload = function () {
+    fetchDogs();
+    setTimeout(showDogNames, 2000);
+}
