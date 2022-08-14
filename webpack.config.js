a52981bc2c09f4dc8b046214b194a36537d25ebc
JasonRoossens <jason.roossens@student.ehb.be>
2022-08-14 12:14:00 +0200

    webpack update

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,20 @@
 const path = require('path');
 
+// entry: ['./src/script.js', './src/breeds.js'],
+
 module.exports = {
+
     entry: {
         index: './src/script.js',
         breeds: './src/breeds.js'
     },
+
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'docs'),
 
     },
+
     mode: 'production',
     watch: true
 };
