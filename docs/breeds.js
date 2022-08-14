ab47f798e711960c9b3d3e336604325d9cc8ce1b
JasonRoossens <jason.roossens@student.ehb.be>
2022-08-15 00:23:06 +0200

    webpack update

--- docs/breeds.js
+++ docs/breeds.js
@@ -1,1 +1,1 @@
-(async()=>{const e=await fetch("https://api.thedogapi.com/v1/breeds");(e=>{const t=document.querySelector(".breed-select");e.map((e=>{const t=document.createElement("option");return t.text=e.name,t.value=e.id,t})).forEach((e=>{t.appendChild(e)}))})(await e.json())})();
+(async()=>{const e=await fetch("https://api.thedogapi.com/v1/breeds");(e=>{const t=document.querySelector(".breed-select");e.map((e=>{const t=document.createElement("option");return t.text=e.name,t.value=e.id,t})).forEach((e=>{t.appendChild(e)}))})(await e.json())})(),console.log("webpack works");
--- src/breeds.js
+++ src/breeds.js
@@ -105,3 +105,5 @@ const changeDog = () => {
 }
 
 fetchDogBreeds();
+
+console.log('webpack works')
